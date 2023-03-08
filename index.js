5d3a29862c62f6d96b5377c0dbde34a29e4a32a4
Dare <dspolk@gmail.com>
2023-03-07 19:54:41 -0800

    complete

---
+++ index.js
@@ -0,0 +1,25 @@
+function subtract(b, a) {
+    return b - a
+}
+function add(a, b) {
+    return a + b
+}
+function multiply(a, b) {
+    return a * b
+}
+function divide(a, b) {
+    return a / b
+}
+function increment(a) {
+    return a + 1
+}
+function decrement(a) {
+    return a - 1
+}
+function makeInt(string) {
+     return parseInt(string, "10")
+    return (string)
+}
+function preserveDecimal(string) {
+    return parseFloat(string)
+    }
